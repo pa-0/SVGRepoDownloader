d9893241afada9e18bf28852e49ea5caee1c82e8
Jesper Gustin <sirjeppe@gmail.com>
2024-12-08 12:41:53 +0100

    Fix issue #1 reported by travelalpaca

--- download.py
+++ download.py
@@ -5,10 +5,33 @@ import requests
 import re
 
 
+class SVGFile:
+    url: str
+    _path: str
+
+    def __init__(self, url: str, path: str):
+        self.url = url
+        self._path = path
+
+    def download(self) -> None:
+        r = requests.get(self.url, allow_redirects=True)
+        if not os.path.exists(self._path):
+            os.makedirs(self._path)
+        with open(self.get_target_path(), 'wb+') as f:
+            f.write(r.content)
+
+    def get_target_path(self):
+        url_segments = self.url.rsplit('/', 2)
+        file_name = url_segments[-1]
+        id = url_segments[-2]
+        file_name_with_id = f'{os.path.splitext(file_name)[0]}.{id}.svg'
+        return os.path.join(self._path, file_name_with_id)
+
+
 class SVGCollectionDownloader:
     _base_url: str = None
     _collection_name: str = None
-    _svg_urls: list[str] = []
+    _svg_files: list[SVGFile] = []
     _path = None
 
     def __init__(self, url: str, path: str):
@@ -19,38 +42,30 @@ class SVGCollectionDownloader:
         self._path = path
         print('Downloading collection from', url)
 
-        self._download()
-    
-    def _download(self):
+    def download(self) -> None:
         page = 1
         next_page = f'{self._base_url}/{self._collection_name}'
         while page > 0:
             print('Gathering download links from', next_page)
             r = requests.get(next_page, allow_redirects=True)
-           
+
             # Match anything "/show/[0-9]+/<some name>.svg"
             matches = re.findall(r'"(\/show\/[0-9]+\/[^"]+\.svg)"', r.text)
             for match in matches:
                 download_url = f'{self._base_url.rsplit("/", 1)[0]}{match.replace("/show/", "/download/")}'
-                if download_url not in self._svg_urls:
-                    self._svg_urls.append(download_url)
-            
+                if download_url not in self._svg_files:
+                    self._svg_files.append(SVGFile(download_url, self._path))
+
             # Check if we have a next page
             page += 1
             if re.search(f'"/collection/{self._collection_name}/{page}"', r.text) is not None:
                 next_page = f'{self._base_url}/{self._collection_name}/{page}'
             else:
                 page = 0
-        
-        for url in self._svg_urls:
-            file_name = url.rsplit('/', 1)[-1]
-            target_path = os.path.join(self._path, file_name)
-            print('Downloading', url, 'to', target_path)
-            r = requests.get(url, allow_redirects=True)
-            if not os.path.exists(self._path):
-                os.makedirs(self._path)
-            with open(target_path, 'wb+') as f:
-                f.write(r.content)
+
+        for svg_file in self._svg_files:
+            print('Downloading', svg_file.url, 'to', svg_file.get_target_path())
+            svg_file.download()
 
 
 if __name__ == '__main__':
@@ -71,3 +86,4 @@ if __name__ == '__main__':
             args.path = os.path.join(os.path.expanduser('~'), 'icons')
 
     svg_collection_downloader = SVGCollectionDownloader(url=args.url, path=args.path)
+    svg_collection_downloader.download()
